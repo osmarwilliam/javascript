d8642ddef99b3820cfb1beae7206841062a98ee0
Osmar William <osmarwilliam@outlook.com>
2024-08-26 18:36:02 -0300

    adicionando aulas do conteúdo inicial

---
+++ Beginners/aula06/index.js
@@ -0,0 +1,19 @@
+let nome;
+nome = 'will';
+console.log(nome);
+nome = 'joao';
+console.log(nome);
+
+/*
+    * nao podemos criar variaveis com palavras reservadas
+        ex: let if or let let
+    * precisam ter nomes significativos
+    * nao pode começar o nome de uma variável com um número
+        ex: let 12nome
+    * nao podem conter espaços ou traços
+        ex: let nome cliente ou let nome-cliente
+    * utilizamos camelCase
+        ex: let nomeCompleto;
+    * são case-sensitive
+        ex: 'let nomeCliente' é diferente de 'let nomecliente'
+*/
---
+++ Beginners/aula06/tests.js
@@ -0,0 +1,6 @@
+let nome = 'pedro'; //string
+
+console.log(nome, "nasceu em ");
+console.log("em 2000", nome, "conheceu maria");
+console.log(nome, "casou-se com maria em 2012");
+console.log("maria teve 1 filho com", nome, "em 2015");
