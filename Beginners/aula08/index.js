ca8c51a5af6a2591c18aa005ca360aee9e913a67
osmarwilliam <osmarwilliam4@gmail.com>
2024-08-27 20:30:11 -0300

    adicionando os materiais da aula 08

--- Beginners/aula08/index.js
+++ Beginners/aula08/index.js
@@ -1,1 +1,17 @@
-console.log("osmar william");
+const nome = "osmar william"
+const sobrenome = "silva"
+const idade = 23;
+const peso = 94;
+const altura = 1.79;
+let imc; // peso / (altura * altura)
+let anoNascimento;
+
+// templates strings
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}Kg`);
+
+imc = peso / (altura * altura);
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
+
+anoNascimento = 2024 - idade;
+console.log(`${nome} nasceu em ${anoNascimento}.`);
