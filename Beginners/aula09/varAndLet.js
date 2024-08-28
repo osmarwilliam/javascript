e9c4ae45433fd820b7f4963eb8ae156ff7cd0d46
osmarwilliam <osmarwilliam4@gmail.com>
2024-08-27 21:53:40 -0300

    conteúdo da aula 09 sobre let x var

---
+++ Beginners/aula09/varAndLet.js
@@ -0,0 +1,9 @@
+var nome = "osmar";
+var nome =  "william"; // usando LET bastaria colocar "nome = 'william'"
+console.log(nome);
+// repare que é possível definir a variável quantas vezes puder com o mesmo nome
+// caso fosse usado o LET, teria dado um error
+let secondName = "arthur";
+secondName = "william"; // se utilizasse let novamente para redefinir daria um erro
+secondName = "osmar"; 
+console.log(secondName);
