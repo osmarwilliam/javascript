4567e386ca53eb1f7423ba0bbde750afd4b71a5c
osmarwilliam <osmarwilliam4@gmail.com>
2024-09-06 15:14:02 -0300

    adicionando aula 12 e o desafio.

---
+++ Beginners/aula12/exercicio.js
@@ -0,0 +1,9 @@
+let num1 = window.prompt("Digite um número: ");
+let num2 = window.prompt("Digite um outro número: ");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+window.alert(`O resultado da soma foi: ${resultado}`);
---
+++ Beginners/aula12/script.js
@@ -0,0 +1,13 @@
+window.alert('Com a nossa mensagem.');
+
+window.confirm('dejesa realmente apagar?'); // retorna true or false 
+
+nome = window.prompt('Digite o seu nome:');
+
+// são três métodos, métodos por estarem dentro do OBJETO window
+// o window.algo, quer dizer que existe um função dentro do objeto window
+// função dentro de objeto. chamamos de MÉTODO, fora de um objeto CHAMAMOS DE FUNÇÃO APENAS
+
+let num1 = prompt('digite um número:');
+
+let num2 = prompt('digite outro número:');
